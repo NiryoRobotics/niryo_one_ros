507c9c138f1573ea1c9e3241cba860e9ac585a00
Sarra ELGHALI <s.elghali@niryo.com>
2018-04-09 17:26:57 +0200

    update matlab_manager.py class

--- niryo_one_user_interface/scripts/matlab_manager.py
+++ niryo_one_user_interface/scripts/matlab_manager.py
@@ -30,8 +30,9 @@ from niryo_one_msgs.msg import RobotMoveAction
 from niryo_one_msgs.msg import RobotMoveGoal
 from niryo_one_msgs.msg import RobotMoveResult
 """
-This class is the main interface between Mtalab and niryo_one 
-it's create a publisher niryo_one_matlab/command to publish joints goal from matlab interface 
+This class is an  interface between Matalab and niryo_one 
+This Class was created as a primitive solution to send Goal form matlab to niryo-one-ros, Instead of sending a Goal using an action client(action client didn't work with matlab) ,the goal will be published by matlab , recived by the subscriber niryo_one_matlab/command and send by an action client created in this class
+it's create a publisher niryo_one_matlab/command to publish joints goal from matlab interface. 
 those goals are sent by the action server client created 
 after executing the command niryo_one_matlab/result publisher publish MatlabMoveResult message  
 """
@@ -41,14 +42,13 @@ class MatlabManager:
         return {'status': status, 'message': message}
     
     def callback_matlab_node(self,request):
-        rospy.loginfo("........................matlab node ........................................")
+        rospy.loginfo("........................receive Matlab Goal  ........................................")
         cmd=request.joints
         cmd_type=request.cmd_type
         status=1
         message="Sueccess"
         response=self.send_matlab_goal(cmd,cmd_type)
         msg=MatlabMoveResult()
-        rospy.loginfo("Matlab node publisher......")
         msg.status=response.status
         msg.message=response.message
         rospy.loginfo(msg)
@@ -59,7 +59,7 @@ class MatlabManager:
     def __init__(self): 
         self.matlab_node=rospy.Subscriber('/niryo_one_matlab/command',RobotMoveCommand,self.callback_matlab_node)  
         self.matlab_node_publisher=rospy.Publisher('/niryo_one_matlab/result',MatlabMoveResult,queue_size=10) 
-        rospy.loginfo('----------Matlab node -------------------------') 
+        rospy.loginfo('..............Matlab OK.................') 
     
     def send_matlab_command(self,commande):
         self.action_client_matlab.send_goal(commande)
@@ -68,7 +68,7 @@ class MatlabManager:
     
     def send_matlab_goal(self,cmd,cmd_type):    
         self.action_client_matlab= actionlib.SimpleActionClient('/niryo_one/commander/robot_action',niryo_one_msgs.msg.RobotMoveAction)
-        rospy.loginfo("waitnig for action server: niryo_one/robot_action....")
+        rospy.loginfo("waiting for action server: niryo_one/robot_action....")
         self.action_client_matlab.wait_for_server()
         rospy.loginfo("Found action server : niryo_one/robot_action")
         goal=RobotMoveGoal()
@@ -77,8 +77,9 @@ class MatlabManager:
         self.action_client_matlab.send_goal(goal)
         rospy.loginfo("waiting for result")
         self.action_client_matlab.wait_for_result()
-        rospy.loginfo("result........")
         response= self.action_client_matlab.get_result()
+        rospy.loginfo("..........result.................")
+        rospy.loginfo(response) 
         result = self.create_response(response.status,response.message)
         return response
 
