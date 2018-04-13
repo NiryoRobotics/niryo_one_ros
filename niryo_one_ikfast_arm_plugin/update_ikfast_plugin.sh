530e346886ebdbaa91b19eda4aca3da29d409bd8
Edouard Renard <edo.renard@gmail.com>
2018-04-13 14:45:20 +0200

    Only use KDL IK solver

--- niryo_one_ikfast_arm_plugin/update_ikfast_plugin.sh
+++
@@ -1,1 +0,0 @@
-rosrun moveit_kinematics create_ikfast_moveit_plugin.py niryo_one arm niryo_one_ikfast_arm_plugin /home/edouard/catkin_ws/src/niryo_one_ikfast_arm_plugin/src/niryo_one_arm_ikfast_solver.cpp
