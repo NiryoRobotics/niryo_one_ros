feb4b6a8f30b8f68b7f180ec38d14794a692223f
Edouard Renard <edo.renard@gmail.com>
2018-07-04 14:00:51 +0200

    Add stl, collada files for hw V2.0 and update urdf, srdf, config files

--- niryo_one_driver/include/niryo_one_driver/niryo_one_hardware_interface.h
+++ niryo_one_driver/include/niryo_one_driver/niryo_one_hardware_interface.h
@@ -50,8 +50,8 @@ class NiryoOneHardwareInterface: public hardware_interface::RobotHW {
         hardware_interface::JointStateInterface joint_state_interface;
         hardware_interface::PositionJointInterface joint_position_interface;
         
-        double cmd[6] = { 0, 1.35, -1.32, 0, 0, 0};
-        double pos[6] = { 0, 1.35, -1.32, 0, 0, 0};
+        double cmd[6] = { 0, 0.64, -1.39, 0, 0, 0};
+        double pos[6] = { 0, 0.64, -1.39, 0, 0, 0};
         double vel[6] = {0};
         double eff[6] = {0};
 
