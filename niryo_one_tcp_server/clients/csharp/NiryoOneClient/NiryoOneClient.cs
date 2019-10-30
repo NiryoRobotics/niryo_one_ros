97855f27c2b76a6ab134e6429bec988bf24e30ed
Rickard Holmberg <rickard.holmberg@raysearchlabs.com>
2019-10-30 18:31:00 +0100

    WIP - CSharpClient

---
+++ niryo_one_tcp_server/clients/csharp/NiryoOneClient/NiryoOneClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace NiryoOneClient
+{
+  public class NiryoOneClient
+  {
+    private TcpClient _client;
+    private int _port;
+    private string _server;
+
+    public NiryoOneClient(string server, int port)
+    {
+      _server = server;
+      _port = port;
+    }
+
+    public void Connect()
+    {
+      if (_client != null)
+      {
+        _client.Close();
+        _client.Dispose();
+      }
+
+      _client = new TcpClient(_server, _port);
+    }
+
+
+  }
+}
