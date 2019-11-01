e3ee9cfcd6542fc4d5d2b253056bee52d8015e7e
Rickard Holmberg <rickard@avkrok.net>
2019-11-01 17:47:29 +0100

    Simplified tests

--- niryo_one_tcp_server/clients/csharp/NiryoOneClient.Tests/NiryoOneConnectionTest.cs
+++ niryo_one_tcp_server/clients/csharp/NiryoOneClient.Tests/NiryoOneConnectionTest.cs
@@ -9,38 +9,49 @@ namespace NiryoOneClient.Tests
     [TestClass]
     public class NiryoOneConnectionTest
     {
+        private TextReader _streamReader;
+        private TextWriter _streamWriter;
+
+        private NiryoOneConnection _connection;
+
+        public NiryoOneConnectionTest()
+        {
+            _streamReader = Substitute.For<System.IO.TextReader>();
+            _streamWriter = Substitute.For<System.IO.TextWriter>();
+            _connection = new NiryoOneConnection(_streamReader, _streamWriter);
+        }
+
         [TestMethod]
         public async Task Calibrate_SuccessfulAuto_Works()
-        {   
-            var streamReader = Substitute.For<System.IO.TextReader>();
-            var streamWriter = Substitute.For<System.IO.TextWriter>();
-            streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:OK\n"));
-            var connection = new NiryoOneConnection(streamReader, streamWriter);
-            await connection.Calibrate(CalibrateMode.AUTO);
-            await streamWriter.Received().WriteLineAsync("CALIBRATE:AUTO");
+        {
+            _streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:OK\n"));
+            await _connection.Calibrate(CalibrateMode.AUTO);
+            await _streamWriter.Received().WriteLineAsync("CALIBRATE:AUTO");
         }
-        
+
         [TestMethod]
         public async Task Calibrate_SuccessfulManual_Works()
-        {   
-            var streamReader = Substitute.For<System.IO.TextReader>();
-            var streamWriter = Substitute.For<System.IO.TextWriter>();
-            streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:OK\n"));
-            var connection = new NiryoOneConnection(streamReader, streamWriter);
-            await connection.Calibrate(CalibrateMode.MANUAL);
-            await streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
-        }        
+        {
+            _streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:OK\n"));
+            await _connection.Calibrate(CalibrateMode.MANUAL);
+            await _streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
+        }
 
         [TestMethod]
         public async Task Calibrate_Failure_Throws()
-        {   
-            var streamReader = Substitute.For<System.IO.TextReader>();
-            var streamWriter = Substitute.For<System.IO.TextWriter>();
-            streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:KO,\"Sucks to be sucky\"\n"));
-            var connection = new NiryoOneConnection(streamReader, streamWriter);
-            var e = await Assert.ThrowsExceptionAsync<NiryoOneException>(async () => await connection.Calibrate(CalibrateMode.MANUAL));
+        {
+            _streamReader.ReadLineAsync().Returns(Task.FromResult("CALIBRATE:KO,\"Sucks to be sucky\"\n"));
+            var e = await Assert.ThrowsExceptionAsync<NiryoOneException>(async () => await _connection.Calibrate(CalibrateMode.MANUAL));
             Assert.AreEqual("Sucks to be sucky", e.Reason);
-            await streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
+            await _streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
+        }
+
+        [TestMethod]
+        public async Task SetLearningMode_True_Works()
+        {
+            _streamReader.ReadLineAsync().Returns(Task.FromResult("SET_LEARNING_MODE:OK"));
+            await _connection.SetLearningMode(true);
+            await _streamWriter.Received().WriteLineAsync("SET_LEARNING_MODE:TRUE");
         }
     }
 }
