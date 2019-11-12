04ac2d72a3c66d5a9cf7bc86e827a674c4505831
Rickard Holmberg <rickard@avkrok.net>
2019-11-12 07:49:07 +0100

    Rename old incorrect test method

--- niryo_one_tcp_server/clients/csharp/NiryoOneClient.Tests/NiryoOneConnectionTest.cs
+++ niryo_one_tcp_server/clients/csharp/NiryoOneClient.Tests/NiryoOneConnectionTest.cs
@@ -54,8 +54,8 @@ namespace NiryoOneClient.Tests
                 new Span<char>(value.ToArray()).CopyTo(buffer.Span);
                 return value.Length;
             }
-        }
 #pragma warning restore 1998 
+        }
 
         private MyTextReader _streamReader;
         private TextWriter _streamWriter;
@@ -69,7 +69,7 @@ namespace NiryoOneClient.Tests
             _connection = new NiryoOneConnection(_streamReader, _streamWriter);
         }
 
-        private void SetupReadLineAsyncReturn_(params string[] values)
+        private void SetupReadAsyncReturn_(params string[] values)
         {
             _streamReader.Values = values;
         }
@@ -77,7 +77,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task Calibrate_SuccessfulAuto_Works()
         {
-            SetupReadLineAsyncReturn_("CALIBRATE:OK");
+            SetupReadAsyncReturn_("CALIBRATE:OK");
             await _connection.Calibrate(CalibrateMode.AUTO);
             await _streamWriter.Received().WriteLineAsync("CALIBRATE:AUTO");
         }
@@ -85,7 +85,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task Calibrate_SuccessfulManual_Works()
         {
-            SetupReadLineAsyncReturn_("CALIBRATE:OK\n");
+            SetupReadAsyncReturn_("CALIBRATE:OK\n");
             await _connection.Calibrate(CalibrateMode.MANUAL);
             await _streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
         }
@@ -93,7 +93,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task Calibrate_Failure_Throws()
         {
-            SetupReadLineAsyncReturn_("CALIBRATE:KO,\"Sucks to be sucky\"");
+            SetupReadAsyncReturn_("CALIBRATE:KO,\"Sucks to be sucky\"");
             var e = await Assert.ThrowsExceptionAsync<NiryoOneException>(async () => await _connection.Calibrate(CalibrateMode.MANUAL));
             Assert.AreEqual("Sucks to be sucky", e.Reason);
             await _streamWriter.Received().WriteLineAsync("CALIBRATE:MANUAL");
@@ -102,7 +102,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetLearningMode_True_Works()
         {
-            SetupReadLineAsyncReturn_("SET_LEARNING_MODE:OK");
+            SetupReadAsyncReturn_("SET_LEARNING_MODE:OK");
             await _connection.SetLearningMode(true);
             await _streamWriter.Received().WriteLineAsync("SET_LEARNING_MODE:TRUE");
         }
@@ -110,7 +110,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetLearningMode_False_Works()
         {
-            SetupReadLineAsyncReturn_("SET_LEARNING_MODE:OK");
+            SetupReadAsyncReturn_("SET_LEARNING_MODE:OK");
             await _connection.SetLearningMode(false);
             await _streamWriter.Received().WriteLineAsync("SET_LEARNING_MODE:FALSE");
         }
@@ -118,7 +118,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetLearningMode_WrongResponse_Throws()
         {
-            SetupReadLineAsyncReturn_("COCO:OK");
+            SetupReadAsyncReturn_("COCO:OK");
             var e = await Assert.ThrowsExceptionAsync<NiryoOneException>(async () => await _connection.SetLearningMode(false));
             Assert.AreEqual("Wrong command response received.", e.Reason);
             await _streamWriter.Received().WriteLineAsync("SET_LEARNING_MODE:FALSE");
@@ -127,7 +127,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetLearningMode_ErrorResponse_Throws()
         {
-            SetupReadLineAsyncReturn_("SET_LEARNING_MODE:KO,\"No good\"");
+            SetupReadAsyncReturn_("SET_LEARNING_MODE:KO,\"No good\"");
             var e = await Assert.ThrowsExceptionAsync<NiryoOneException>(async () => await _connection.SetLearningMode(false));
             Assert.AreEqual("No good", e.Reason);
             await _streamWriter.Received().WriteLineAsync("SET_LEARNING_MODE:FALSE");
@@ -136,7 +136,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task MoveJoints_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("MOVE_JOINTS:OK");
+            SetupReadAsyncReturn_("MOVE_JOINTS:OK");
             await _connection.MoveJoints(new RobotJoints(new[] {
                 0.03f, 0.0123f, 0.456f, 0.987f, 0.654f, 0.321f
             }));
@@ -146,7 +146,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task MovePose_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("MOVE_POSE:OK");
+            SetupReadAsyncReturn_("MOVE_POSE:OK");
             await _connection.MovePose(new PoseObject(new[] {
                 0.03f, 0.0123f, 0.456f, 0.987f, 0.654f, 0.321f
             }));
@@ -156,7 +156,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task ShiftPose_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("SHIFT_POSE:OK");
+            SetupReadAsyncReturn_("SHIFT_POSE:OK");
             await _connection.ShiftPose(RobotAxis.ROLL, 0.03142f);
             await _streamWriter.Received().WriteLineAsync("SHIFT_POSE:ROLL,0.03142");
         }
@@ -164,7 +164,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetArmMaxVelocity_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("SET_ARM_MAX_VELOCITY:OK");
+            SetupReadAsyncReturn_("SET_ARM_MAX_VELOCITY:OK");
             await _connection.SetArmMaxVelocity(50);
             await _streamWriter.Received().WriteLineAsync("SET_ARM_MAX_VELOCITY:50");
         }
@@ -172,7 +172,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task EnableJoystick_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("ENABLE_JOYSTICK:OK");
+            SetupReadAsyncReturn_("ENABLE_JOYSTICK:OK");
             await _connection.EnableJoystick(false);
             await _streamWriter.Received().WriteLineAsync("ENABLE_JOYSTICK:FALSE");
         }
@@ -180,7 +180,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetPinMode_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("SET_PIN_MODE:OK");
+            SetupReadAsyncReturn_("SET_PIN_MODE:OK");
             await _connection.SetPinMode(RobotPin.GPIO_2B, PinMode.OUTPUT);
             await _streamWriter.Received().WriteLineAsync("SET_PIN_MODE:GPIO_2B,OUTPUT");
         }
@@ -188,7 +188,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task DigitalWrite_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("DIGITAL_WRITE:OK");
+            SetupReadAsyncReturn_("DIGITAL_WRITE:OK");
             await _connection.DigitalWrite(RobotPin.GPIO_2A, DigitalState.LOW);
             await _streamWriter.Received().WriteLineAsync("DIGITAL_WRITE:GPIO_2A,LOW");
         }
@@ -196,7 +196,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task DigitalRead_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("DIGITAL_READ:OK,HIGH");
+            SetupReadAsyncReturn_("DIGITAL_READ:OK,HIGH");
             await _connection.DigitalRead(RobotPin.GPIO_1A);
             await _streamWriter.Received().WriteLineAsync("DIGITAL_READ:GPIO_1A");
         }
@@ -204,7 +204,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task ChangeTool_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("CHANGE_TOOL:OK");
+            SetupReadAsyncReturn_("CHANGE_TOOL:OK");
             await _connection.ChangeTool(RobotTool.GRIPPER_2);
             await _streamWriter.Received().WriteLineAsync("CHANGE_TOOL:GRIPPER_2");
         }
@@ -212,7 +212,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task OpenGripper_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("OPEN_GRIPPER:OK");
+            SetupReadAsyncReturn_("OPEN_GRIPPER:OK");
             await _connection.OpenGripper(RobotTool.GRIPPER_1, 200);
             await _streamWriter.Received().WriteLineAsync("OPEN_GRIPPER:GRIPPER_1,200");
         }
@@ -220,7 +220,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task CloseGripper_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("CLOSE_GRIPPER:OK");
+            SetupReadAsyncReturn_("CLOSE_GRIPPER:OK");
             await _connection.CloseGripper(RobotTool.GRIPPER_1, 200);
             await _streamWriter.Received().WriteLineAsync("CLOSE_GRIPPER:GRIPPER_1,200");
         }
@@ -228,7 +228,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task PullAirVacuumPump_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("PULL_AIR_VACUUM_PUMP:OK");
+            SetupReadAsyncReturn_("PULL_AIR_VACUUM_PUMP:OK");
             await _connection.PullAirVacuumPump(RobotTool.VACUUM_PUMP_1);
             await _streamWriter.Received().WriteLineAsync("PULL_AIR_VACUUM_PUMP:VACUUM_PUMP_1");
         }
@@ -236,7 +236,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task PushAirVacuumPump_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("PUSH_AIR_VACUUM_PUMP:OK");
+            SetupReadAsyncReturn_("PUSH_AIR_VACUUM_PUMP:OK");
             await _connection.PushAirVacuumPump(RobotTool.VACUUM_PUMP_1);
             await _streamWriter.Received().WriteLineAsync("PUSH_AIR_VACUUM_PUMP:VACUUM_PUMP_1");
         }
@@ -244,7 +244,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task SetupElectromagnet_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("SETUP_ELECTROMAGNET:OK");
+            SetupReadAsyncReturn_("SETUP_ELECTROMAGNET:OK");
             await _connection.SetupElectromagnet(RobotTool.ELECTROMAGNET_1, RobotPin.GPIO_2B);
             await _streamWriter.Received().WriteLineAsync("SETUP_ELECTROMAGNET:ELECTROMAGNET_1,GPIO_2B");
         }
@@ -252,7 +252,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task ActivateElectromagnet_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("ACTIVATE_ELECTROMAGNET:OK");
+            SetupReadAsyncReturn_("ACTIVATE_ELECTROMAGNET:OK");
             await _connection.ActivateElectromagnet(RobotTool.ELECTROMAGNET_1, RobotPin.GPIO_2B);
             await _streamWriter.Received().WriteLineAsync("ACTIVATE_ELECTROMAGNET:ELECTROMAGNET_1,GPIO_2B");
         }
@@ -260,7 +260,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task DeactivateElectromagnet_Sample_SendsCorrectly()
         {
-            SetupReadLineAsyncReturn_("DEACTIVATE_ELECTROMAGNET:OK");
+            SetupReadAsyncReturn_("DEACTIVATE_ELECTROMAGNET:OK");
             await _connection.DeactivateElectromagnet(RobotTool.ELECTROMAGNET_1, RobotPin.GPIO_2C);
             await _streamWriter.Received().WriteLineAsync("DEACTIVATE_ELECTROMAGNET:ELECTROMAGNET_1,GPIO_2C");
         }
@@ -268,7 +268,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task GetJoints_Sample_Works()
         {
-            SetupReadLineAsyncReturn_("GET_JOINTS:OK,0.0,0.640187,-1.397485,0.0,0.0,0.0");
+            SetupReadAsyncReturn_("GET_JOINTS:OK,0.0,0.640187,-1.397485,0.0,0.0,0.0");
             var joints = await _connection.GetJoints();
             CollectionAssert.AreEqual(new[] { 0.0f, 0.640187f, -1.397485f, 0.0f, 0.0f, 0.0f }, joints.ToArray());
         }
@@ -276,7 +276,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task GetPose_Sample_Works()
         {
-            SetupReadLineAsyncReturn_("GET_POSE:OK,0.0695735635306,1.31094787803e-12,0.200777981243,-5.10302119597e-12,0.757298,5.10351727471e-12");
+            SetupReadAsyncReturn_("GET_POSE:OK,0.0695735635306,1.31094787803e-12,0.200777981243,-5.10302119597e-12,0.757298,5.10351727471e-12");
             var pose = await _connection.GetPose();
             CollectionAssert.AreEqual(new[] { 0.0695735635306f, 1.31094787803e-12f, 0.200777981243f, -5.10302119597e-12f, 0.757298f, 5.10351727471e-12f },
                 pose.ToArray());
@@ -285,7 +285,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task GetHardwareStatus_Sample_Works()
         {
-            SetupReadLineAsyncReturn_("GET_HARDWARE_STATUS:OK,59,2,True,'',0,False,['Stepper Axis 1', 'Stepper Axis 2', 'Stepper Axis 3', 'Servo Axis 4', 'Servo Axis 5', 'Servo Axis 6'],['Niryo Stepper', 'Niryo Stepper', 'Niryo Stepper', 'DXL XL-430', 'DXL XL-430', 'DXL XL-320'],(34, 34, 37, 43, 45, 37),(0.0, 0.0, 0.0, 11.3, 11.2, 7.9),(0, 0, 0, 0, 0, 0)");
+            SetupReadAsyncReturn_("GET_HARDWARE_STATUS:OK,59,2,True,'',0,False,['Stepper Axis 1', 'Stepper Axis 2', 'Stepper Axis 3', 'Servo Axis 4', 'Servo Axis 5', 'Servo Axis 6'],['Niryo Stepper', 'Niryo Stepper', 'Niryo Stepper', 'DXL XL-430', 'DXL XL-430', 'DXL XL-320'],(34, 34, 37, 43, 45, 37),(0.0, 0.0, 0.0, 11.3, 11.2, 7.9),(0, 0, 0, 0, 0, 0)");
             var status = await _connection.GetHardwareStatus();
             Assert.AreEqual(59, status.RpiTemperature);
             Assert.AreEqual(2, status.HardwareVersion);
@@ -303,7 +303,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task GetLearningMode_Sample_Works()
         {
-            SetupReadLineAsyncReturn_("GET_LEARNING_MODE:OK,FALSE");
+            SetupReadAsyncReturn_("GET_LEARNING_MODE:OK,FALSE");
             var mode = await _connection.GetLearningMode();
             await _streamWriter.Received().WriteLineAsync("GET_LEARNING_MODE");
             Assert.AreEqual(false, mode);
@@ -312,7 +312,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task GetDigitalIOState_Sample_Works()
         {
-            SetupReadLineAsyncReturn_("GET_DIGITAL_IO_STATE:OK,[2, '1A', 1, 1],[3, '1B', 1, 1],[16, '1C', 1, 1],[26, '2A', 1, 1],[19, '2B', 1, 1],[6, '2C', 1, 1],[12, 'SW1', 0, 0],[13, 'SW2', 0, 0]");
+            SetupReadAsyncReturn_("GET_DIGITAL_IO_STATE:OK,[2, '1A', 1, 1],[3, '1B', 1, 1],[16, '1C', 1, 1],[26, '2A', 1, 1],[19, '2B', 1, 1],[6, '2C', 1, 1],[12, 'SW1', 0, 0],[13, 'SW2', 0, 0]");
             var state = await _connection.GetDigitalIOState();
             Assert.AreEqual(2, state[0].PinId);
             Assert.AreEqual("1A", state[0].Name);
@@ -357,7 +357,7 @@ namespace NiryoOneClient.Tests
 
         [TestMethod]
         public async Task ReceiveAnswerAsync_GroupedInOneRead_Works() {
-            SetupReadLineAsyncReturn_("HEJ:OKBLA:OKMERP:OK");
+            SetupReadAsyncReturn_("HEJ:OKBLA:OKMERP:OK");
             var a1 = await _connection.ReceiveAnswerAsync("HEJ");
             Assert.AreEqual("", a1);
             var a2 = await _connection.ReceiveAnswerAsync("BLA");
@@ -369,7 +369,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task ReceiveAnswerAsync_SplitInMultipleReads_Works() 
         {
-            SetupReadLineAsyncReturn_("HEJ:O", "KBLA:", "OKMERP", ":OK");
+            SetupReadAsyncReturn_("HEJ:O", "KBLA:", "OKMERP", ":OK");
             var a1 = await _connection.ReceiveAnswerAsync("HEJ");
             Assert.AreEqual("", a1);
             var a2 = await _connection.ReceiveAnswerAsync("BLA");
@@ -381,7 +381,7 @@ namespace NiryoOneClient.Tests
         [TestMethod]
         public async Task ReceiveAnswerAsync_ArbitraryWhitespaceBetween_Works() 
         {
-            SetupReadLineAsyncReturn_("HEJ:OK\n\t BLA:OK \t\r\nMERP:OK\r\n");
+            SetupReadAsyncReturn_("HEJ:OK\n\t BLA:OK \t\r\nMERP:OK\r\n");
             var a1 = await _connection.ReceiveAnswerAsync("HEJ");
             Assert.AreEqual("", a1);
             var a2 = await _connection.ReceiveAnswerAsync("BLA");
